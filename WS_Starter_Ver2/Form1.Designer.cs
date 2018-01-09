d28a05cf35cc4155ba5f7353b5e0478a9e60830b
Jan Fruseth <jan.fruseth@bisnode.com>
2018-01-09 08:27:26 +0100

    Initialize project

---
+++ WS_Starter_Ver2/Form1.Designer.cs
@@ -0,0 +1,261 @@
+﻿namespace WS_Starter_Ver2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGetData = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.dgvResponse = new System.Windows.Forms.DataGridView();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtCompanyName = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.btnGetReellerettighetshavere = new System.Windows.Forms.Button();
+            this.dgvReellerettighetshavere = new System.Windows.Forms.DataGridView();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxOrgnrReellerettighetshavere = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvResponse)).BeginInit();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvReellerettighetshavere)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnGetData
+            // 
+            this.btnGetData.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnGetData.Location = new System.Drawing.Point(838, 39);
+            this.btnGetData.Name = "btnGetData";
+            this.btnGetData.Size = new System.Drawing.Size(207, 23);
+            this.btnGetData.TabIndex = 4;
+            this.btnGetData.Text = "Søk";
+            this.btnGetData.UseVisualStyleBackColor = true;
+            this.btnGetData.Click += new System.EventHandler(this.btnGetData_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Username:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Password";
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(103, 12);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(100, 20);
+            this.txtUsername.TabIndex = 0;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(103, 41);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(100, 20);
+            this.txtPassword.TabIndex = 1;
+            this.txtPassword.UseSystemPasswordChar = true;
+            // 
+            // dgvResponse
+            // 
+            this.dgvResponse.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvResponse.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvResponse.Location = new System.Drawing.Point(6, 68);
+            this.dgvResponse.Name = "dgvResponse";
+            this.dgvResponse.Size = new System.Drawing.Size(1039, 421);
+            this.dgvResponse.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 52);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Hit list:";
+            // 
+            // txtCompanyName
+            // 
+            this.txtCompanyName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtCompanyName.Location = new System.Drawing.Point(97, 13);
+            this.txtCompanyName.Name = "txtCompanyName";
+            this.txtCompanyName.Size = new System.Drawing.Size(948, 20);
+            this.txtCompanyName.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 16);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(85, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Company Name:";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(24, 77);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1059, 521);
+            this.tabControl1.TabIndex = 10;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.label4);
+            this.tabPage1.Controls.Add(this.btnGetData);
+            this.tabPage1.Controls.Add(this.txtCompanyName);
+            this.tabPage1.Controls.Add(this.dgvResponse);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1051, 495);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Firmasøk";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.btnGetReellerettighetshavere);
+            this.tabPage2.Controls.Add(this.dgvReellerettighetshavere);
+            this.tabPage2.Controls.Add(this.label5);
+            this.tabPage2.Controls.Add(this.textBoxOrgnrReellerettighetshavere);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1051, 495);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Reellerettighetshavere";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // btnGetReellerettighetshavere
+            // 
+            this.btnGetReellerettighetshavere.Location = new System.Drawing.Point(172, 12);
+            this.btnGetReellerettighetshavere.Name = "btnGetReellerettighetshavere";
+            this.btnGetReellerettighetshavere.Size = new System.Drawing.Size(75, 23);
+            this.btnGetReellerettighetshavere.TabIndex = 7;
+            this.btnGetReellerettighetshavere.Text = "Hent data";
+            this.btnGetReellerettighetshavere.UseVisualStyleBackColor = true;
+            this.btnGetReellerettighetshavere.Click += new System.EventHandler(this.btnGetReellerettighetshavere_Click);
+            // 
+            // dgvReellerettighetshavere
+            // 
+            this.dgvReellerettighetshavere.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvReellerettighetshavere.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvReellerettighetshavere.Location = new System.Drawing.Point(6, 68);
+            this.dgvReellerettighetshavere.Name = "dgvReellerettighetshavere";
+            this.dgvReellerettighetshavere.Size = new System.Drawing.Size(1039, 421);
+            this.dgvReellerettighetshavere.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(7, 17);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(36, 13);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Orgnr:";
+            // 
+            // textBoxOrgnrReellerettighetshavere
+            // 
+            this.textBoxOrgnrReellerettighetshavere.Location = new System.Drawing.Point(49, 14);
+            this.textBoxOrgnrReellerettighetshavere.Name = "textBoxOrgnrReellerettighetshavere";
+            this.textBoxOrgnrReellerettighetshavere.Size = new System.Drawing.Size(117, 20);
+            this.textBoxOrgnrReellerettighetshavere.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnGetData;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1101, 633);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Starter";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvResponse)).EndInit();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvReellerettighetshavere)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGetData;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.DataGridView dgvResponse;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtCompanyName;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button btnGetReellerettighetshavere;
+        private System.Windows.Forms.DataGridView dgvReellerettighetshavere;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxOrgnrReellerettighetshavere;
+    }
+}
+
---
+++ WS_Starter_Ver2/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WS_Starter_Ver2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+        private void btnGetData_Click(object sender, EventArgs e)
+        {
+            btnGetData.Enabled = false;
+            this.Cursor = Cursors.WaitCursor;
+
+            try
+            {
+                var brukerAutoriasasjon = new WS_Foretak.BrukerAutorisasjon
+                {
+                    BrukerID = txtUsername.Text,
+                    Passord = txtPassword.Text
+                };
+
+                var foretak = new WS_Foretak.Foretak {BrukerAutorisasjonValue = brukerAutoriasasjon};
+                var fs = new WS_Foretak.ForetakSok {Navn = txtCompanyName.Text};
+                var fsResponse = foretak.sokForetak(fs);
+
+                if (fsResponse.ForetakData != null)
+                {
+                    dgvResponse.DataSource = fsResponse.ForetakData;
+                }
+
+                var sbMessageSummary = new StringBuilder();
+                if (fsResponse.Meldinger != null)
+                {
+                    var i = 1;
+                    foreach (var message in fsResponse.Meldinger)
+                    {
+                        sbMessageSummary.AppendLine(
+                            $"Message nr {i++}: {message.MeldingsKode} {message.MeldingsTekst} ");
+                    }
+                }
+
+                this.Cursor = Cursors.Default;
+
+                if (sbMessageSummary.Length > 1)
+                {
+                    MessageBox.Show(sbMessageSummary.ToString());
+                }
+            }
+            catch (Exception ex)
+            {
+                this.Cursor = Cursors.Default;
+                MessageBox.Show($"Errormessage: {ex.Message}");
+            }
+            finally
+            {
+                btnGetData.Enabled = true;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnGetReellerettighetshavere_Click(object sender, EventArgs e)
+        {
+            btnGetReellerettighetshavere.Enabled = false;
+            this.Cursor = Cursors.WaitCursor;
+
+            try
+            {
+                var brukerAutoriasasjon = new WS_Foretak.BrukerAutorisasjon
+                {
+                    BrukerID = txtUsername.Text,
+                    Passord = txtPassword.Text
+                };
+
+                var foretak = new WS_Foretak.Foretak {BrukerAutorisasjonValue = brukerAutoriasasjon};
+                var hf = new WS_Foretak.HentForetak
+                {
+                    Orgnr = int.Parse(textBoxOrgnrReellerettighetshavere.Text),
+                    ReelleRettighetshavere = true
+                };
+
+                var hfResponse = foretak.hentForetakinfo(hf);
+
+                if (hfResponse.Rettighetshavere != null)
+                {
+                    dgvReellerettighetshavere.DataSource = hfResponse.Rettighetshavere;
+                }
+
+                var sbMessageSummary = new StringBuilder();
+
+                if (hfResponse.Meldinger != null)
+                {
+                    var i = 1;
+                    foreach (var message in hfResponse.Meldinger)
+                    {
+                        sbMessageSummary.AppendLine(
+                            $"Message nr {i++}: {message.MeldingsKode} {message.MeldingsTekst} ");
+                    }
+                }
+
+                this.Cursor = Cursors.Default;
+
+                if (sbMessageSummary.Length > 1)
+                {
+                    MessageBox.Show(sbMessageSummary.ToString());
+                }
+            }
+            catch (Exception ex)
+            {
+                this.Cursor = Cursors.Default;
+                MessageBox.Show($"Errormessage: {ex.Message}");
+            }
+            finally
+            {
+                btnGetReellerettighetshavere.Enabled = true;
+            }
+        }
+    }
+}
